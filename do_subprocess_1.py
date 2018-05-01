ebec3e79be4874e8ceddc18590d3bae19484dcb9
quanqiliang <quanqiliang@yahoo.com>
2018-05-01 23:27:06 +0800

    add process files

---
+++ do_subprocess_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+'''
+如果子进程还需要输入，则可以通过communicate()方法输入：
+'''
+print('$ nslookup')
+p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+print(output.decode('utf-8'))
+print('Exit code:', p.returncode)
+
+'''
+上面的代码相当于在命令行执行命令nslookup，然后手动输入：
+
+set q=mx
+python.org
+exit
+'''
